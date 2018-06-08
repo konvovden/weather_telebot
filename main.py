2c81d6d1b470398c71cdd886bcad98204115fa1e
konvovden <krasidze1@mail.ru>
2018-06-08 12:53:16 +0400

    more getweathertext

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import json
 
 weather_key = 'b23a723ded22b73ca842f89940aac7d0'
 telegram_key = '617530729:AAGS4jp0eHv5jAvIkvzbr5TGmTcriIqUzzw'
+yandex_translate_key = 'trnsl.1.1.20180608T081728Z.5684da981c2a602d.7822398cfcda6dbcde646aa60813bcac2478b205'
 
 
-# http://api.openweathermap.org/data/2.5/weather?lat=4&lon=139&appid=b23a723ded22b73ca842f89940aac7d0
+
+# http://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180608T081728Z.5684da981c2a602d.7822398cfcda6dbcde646aa60813bcac2478b205&text=weather%20clear&lang=en-ru&format=plain
 
 def GetWeatherMessage(lat, lon) -> str:
 	response = requests.get('http://api.openweathermap.org/data/2.5/weather?lat=' + str(lat) + '&lon=' + str(lon) + '&appid=' + weather_key)
@@ -13,13 +15,22 @@ def GetWeatherMessage(lat, lon) -> str:
 	weather = json.loads(response.text)
 	print(weather['coord'])
 
+	response = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key='+yandex_translate_key+'&lang=eu-ru&text=weather%20' + weather['weather'][0]['main'])
+	# weather_text = json.loads(response.text)
+	# weather_text = weather_text['text']
+	weather_text = json.loads(response.text)
+	weather_text = str(weather_text['text'][0]).replace('погода', '').replace(' ', '')
 	returned_string = ''
 
 	returned_string += 'Географические координаты:\n'
 	returned_string += 'Широта: ' + str(lat) + '. '
-	returned_string += 'Долгота: ' + str(lon) + '.\nn'
-	returned_string += 'Погода: ' + str(weather['weather'][0]['main'])
+	returned_string += 'Долгота: ' + str(lon) + '.\n'
+	returned_string += 'Погода: ' + weather_text + '\n'
+	returned_string += 'Температура: ' + str(int(weather['main']['temp'] - 273)) + '°\n'
+	returned_string += 'Давление: ' + str(int(weather['main']['pressure']*0.750062)) + 'мм. рт. ст (' + str(int(weather['main']['pressure'])) + ' гПа)\n'
+	returned_string += 'Влажность: ' + str(int(weather['main']['humidity'])) + '%\n'
+	returned_string += 'Скорость ветра: ' + str(weather['wind']['speed']) + ' м/с'
+ 
 	return returned_string
-
 
 
